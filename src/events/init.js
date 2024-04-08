3b7b8b410df0b5f15b52dec4e8ad61b440742ca0
Almario Miano <almario.dev@gmail.com>
2024-04-08 13:12:38 -0500

    Create init.js

---
+++ src/events/init.js
@@ -0,0 +1,15 @@
+import { canvas, customUserEvents } from "../utils/variables";
+import mousedown from "./mousedown";
+
+export default function init() {
+  // begin mouse click
+  canvas.element.addEventListener("mousedown", function (e) {
+    Object.entries(mousedown).forEach(([fn, cb]) => {
+      const value = cb(e);
+
+      if (customUserEvents.hasOwnProperty(fn)) {
+        customUserEvents[fn].forEach((customFn) => customFn(value));
+      }
+    });
+  });
+}

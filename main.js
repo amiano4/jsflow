3efd5e08ed5787a55a69e99717b161e0b380cd7b
Almario Miano <almario.dev@gmail.com>
2024-04-08 13:12:05 -0500

    create user event hook

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 import create from "./src/create";
+import { canvas, customUserEvents } from "./src/utils/variables";
 
 const app = document.querySelector("#app");
 
+const on = (hook, callback) => {
+  !customUserEvents[hook] && (customUserEvents[hook] = []);
+  customUserEvents[hook].push(callback);
+};
+
 create(app);
+
+const JsFlow = {
+  on,
+  create,
+};
+
+export default JsFlow;

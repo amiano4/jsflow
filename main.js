33d41dac988897e19fc9950be5d1f14350900779
Almario Miano <almario.dev@gmail.com>
2024-04-16 08:14:31 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 import create from "./src/create";
+import { createStyle, jfMarkup } from "./src/utils/element";
 import { canvas, customUserEvents } from "./src/utils/variables";
+import "remixicon/fonts/remixicon.css";
+import "./src/style.css";
 
 const app = document.querySelector("#app");
+const jf = jfMarkup();
+const style = createStyle();
+const diagramContainer = jf.querySelector(".jfContentDiagramWrap");
+
+app.innerHTML = "";
+document.head.appendChild(style);
+app.appendChild(jf);
 
 const on = (hook, callback) => {
   !customUserEvents[hook] && (customUserEvents[hook] = []);
   customUserEvents[hook].push(callback);
 };
 
-create(app);
+create(diagramContainer);
 
 const JsFlow = {
   on,
